600c31de862972ca07cdf9f355f2cad05db9937c
unknown <Majdmark552@gmail.com>
2023-09-23 03:54:19 +0300

    Apple Store From Js

---
+++ index.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.img0');
+let container = document.querySelector('.container')
+function Phones (Phone)
+{
+img.src = Phone;
+}
+function Colors (color) 
+{
+    container.style.background = color;
+
+}
+
